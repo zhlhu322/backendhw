9c4011476729ca30a753c58189a6008b1f927bc0
Enya <0322enter@gmail.com>
2025-09-20 14:52:27 +0800

    setting i18n,timezone

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module Backendhw
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
-
+    config.i18n.default_locale = "zh-TW"
+    config.time_zone = "Taipei"
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
