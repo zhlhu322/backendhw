c8ada758fac501187671bce82e3c1781a4a3d34f
Enya <0322enter@gmail.com>
2025-09-22 00:17:55 +0800

    Adding missions helper

--- app/helpers/missions_helper.rb
+++ app/helpers/missions_helper.rb
@@ -1,2 +1,14 @@
 module MissionsHelper
+  def render_error(f, column)
+    field_html = capture do
+      yield
+    end
+
+    error_html = ""
+    if f.object.errors[column].any?
+      error_html = content_tag(:span, f.object.errors[column].first, class: "error")
+    end
+
+    field_html + error_html
+  end
 end
