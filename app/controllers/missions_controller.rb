be0a66202be358281b4b008cf9da493abb2db241
Enya <0322enter@gmail.com>
2025-10-06 03:06:45 +0800

    add_tag_by_name

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -13,9 +13,10 @@ class MissionsController < ApplicationController
   end
 
   def create
-    @mission = current_user.missions.new(mission_params)
+    @mission = current_user.missions.new(mission_params.except(:tag_name))
 
     if @mission.save
+      @mission.add_tag_by_name(mission_params[:tag_name])
       flash[:notice] = t("missions.create.success")
       redirect_to mission_path(@mission)
     else
@@ -28,10 +29,12 @@ class MissionsController < ApplicationController
   end
 
   def update
-    if current_mission.update(mission_params)
+    if current_mission.update(mission_params.except(:tag_name))
+      current_mission.add_tag_by_name(mission_params[:tag_name])
       flash[:notice] = t("missions.edit.success")
       redirect_to mission_path(current_mission)
     else
+      puts "Validation Failed: #{current_mission.errors.full_messages}"
       render :edit, status: :unprocessable_entity
     end
   end
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -5,8 +5,7 @@ class Mission < ApplicationRecord
   validates :end_date, comparison: { greater_than: Time.current, message: "must be after the creation date" }
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
-  attr_accessor :tag_name
-  before_save :process_tag_name
+  accepts_nested_attributes_for :taggings, allow_destroy: true
 
   enum :state, {
     pending: "pending",
@@ -20,11 +19,17 @@ class Mission < ApplicationRecord
     high: 2
   }
 
-  def process_tag_name
-    name_to_use = self.tag_name.to_s.strip
-    return if name_to_use.blank?
-    tag_instance = Tag.find_or_create_by!(name: name_to_use)
-    self.tags << tag_instance unless self.tags.include?(tag_instance)
+  # def process_tag_name
+  #   name_to_use = self.tag_name.to_s.strip
+  #   return if name_to_use.blank?
+  #   tag_instance = Tag.find_or_create_by!(name: name_to_use)
+  #   self.tags << tag_instance unless self.tags.include?(tag_instance)
+  # end
+
+  def add_tag_by_name(tag_name)
+    return unless tag_name.present?
+    tag_instance = Tag.find_or_create_by!(name: tag_name.strip)
+    tags << tag_instance if tags.exclude?(tag_instance)
   end
 
   def self.search(query, sort_key = nil)
